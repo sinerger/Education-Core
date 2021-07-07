20fa27c78374d2c346e1e496f38f998927b72ed2
Ola <oleksandrakondratenko@gmail.com>
2021-07-07 14:55:04 +0300

    Create UserDetail

---
+++ Domain/Interfaces/IDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces.UserRepositoryInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Text;
+
+namespace Domain.Interfaces
+{
+    public interface IDBContext
+    {
+        IUserWithRoleRepository UserWithRoleRepository { get; }
+        IUserDetailRepository UserDetailRepository { get; }
+
+        IDbConnection DBConnection { get; }
+
+    }
+}
---
+++ WebApi/Controllers/UserDetailController.cs
@@ -0,0 +1,54 @@
+﻿using Domain.Entities.Users;
+
+using Domain.Interfaces.UserRepositoryInterfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Domain.Interfaces;
+
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserDetailController : ControllerBase
+    {
+        private readonly IDBContext _DBContext;
+
+        public UserDetailController(IDBContext dbContext)
+        {
+            _DBContext = dbContext;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<UserDetail>> GetAllUsersDetail()
+        {
+            return await _DBContext.UserDetailRepository.GetAllUsersDetail();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<UserDetail> GetUserDetailByID (Guid id)
+        {
+            return await _DBContext.UserDetailRepository.GetUserDetailByID(id);
+        }
+
+        [HttpPost]
+        public async Task<bool> CreateUserDetail(UserDetail user)
+        {
+            return _DBContext.UserDetailRepository.CreateUserDetail(user).Result;
+        }
+
+        [HttpPut]
+        public async Task<bool> UpdateUserDetail(UserDetail user)
+        {
+            return _DBContext.UserDetailRepository.UpdateUserDetail(user).Result;
+        }
+        
+        [HttpDelete]
+        public async Task<bool> DeleteUserDetailByID(int id)
+        {
+            return _DBContext.UserDetailRepository.DeleteUserDetailByID(id).Result;
+        }
+    }
+}
