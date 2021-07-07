f0f25dc64dab581eea04f9df5f7d3e5d6f2c0489
sinerger <skochmen@gmail.com>
2021-07-07 22:01:55 +0300

    fixup! fixup! Merge branch 'dev' into ResearchPatternUnitOfWork

--- WebApi/Controllers/UserWithRoleController.cs
+++ WebApi/Controllers/UserWithRoleController.cs
@@ -1,5 +1,5 @@
 ﻿using Domain.Entities.Users;
-using Domain.Interfaces.UserRepositoryInterfaces;
+using Domain.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -13,47 +13,47 @@ namespace WebApi.Controllers
     [ApiController]
     public class UserWithRoleController : ControllerBase
     {
-        private readonly IUserWithRoleUnitOfWork _userWithRoleUnitOfWork;
+        private readonly IDBContext _dbContext;
 
-        public UserWithRoleController(IUserWithRoleUnitOfWork userWithRoleUnitOfWork)
+        public UserWithRoleController(IDBContext dbContext)
         {
-            _userWithRoleUnitOfWork = userWithRoleUnitOfWork;
+            _dbContext = dbContext;
         }
 
         [HttpGet]
         public async Task<IEnumerable<UserWithRole>> GetUsersWithRole()
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.GetUsersWithRoleAsync();
+            return await _dbContext.UserWithRoleRepository.GetUsersWithRoleAsync();
         }
 
         [HttpGet("id")]
         public async Task<UserWithRole> GetUserWithRoleById(Guid id)
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.GetUserWithRoleByIDAsync(id);
+            return await _dbContext.UserWithRoleRepository.GetUserWithRoleByIDAsync(id);
         }
 
         [HttpGet("login")]
-        public async Task<UserWithRole> GetUserWithRoleByLoginAndPassword(string login,string password)
+        public async Task<UserWithRole> GetUserWithRoleByLoginAndPassword(string login, string password)
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.GetUserWithRoleByLoginAndPasswordAsync(login, password);
+            return await _dbContext.UserWithRoleRepository.GetUserWithRoleByLoginAndPasswordAsync(login, password);
         }
 
         [HttpPost]
         public async Task<bool> CreateUserWithRole(UserWithRole user)
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.CreateUserWithRoleAsync(user);
+            return await _dbContext.UserWithRoleRepository.CreateUserWithRoleAsync(user);
         }
 
         [HttpDelete("id")]
         public async Task<bool> DeleteUserWithRole(Guid id)
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.DeleteUserWithRoleAsync(id);
+            return await _dbContext.UserWithRoleRepository.DeleteUserWithRoleAsync(id);
         }
 
         [HttpPut]
         public async Task<bool> UpdateUserWithRole(UserWithRole user)
         {
-            return await _userWithRoleUnitOfWork.UserWithRoleRepository.UpdateUserWithRoleAsync(user);
+            return await _dbContext.UserWithRoleRepository.UpdateUserWithRoleAsync(user);
         }
     }
 }
