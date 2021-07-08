8bd80de84763f885c250d3606db869ab932836bf
Matvijivan <matvijivan>
2021-07-08 12:35:02 +0300

    Fix attribute

--- Domain/Interfaces/CourseRepositoryIntarfaces/ICourseProgramRepository.cs
+++ Domain/Interfaces/CourseRepositoryIntarfaces/ICourseProgramRepository.cs
@@ -9,19 +9,16 @@ namespace Domain.Interfaces.CourseRepositoryIntarfaces
 {
     public interface ICourseProgramRepository : IRepository
     {
-        [Recordset(typeof(Course), typeof(Lesson), typeof(CourseProgram))]
+        [Recordset(1, typeof(Course), typeof(Lesson), IsChild = true, Into = "Lesson")]
         Task<CourseProgram> GetCourseProgramByIDAsync(Guid id);
 
-        [Recordset(typeof(Course), typeof(Lesson), typeof(CourseProgram))]
+        [Recordset(1, typeof(Course), typeof(Lesson), IsChild = true, Into = "Lesson")]
         Task<IEnumerable<CourseProgram>> GetAllCourseProgramAsync();
 
-        [Recordset(typeof(Course), typeof(Lesson), typeof(CourseProgram))]
         Task<bool> CreateCourseProgramAsync(CourseProgram courseProgram);
 
-        [Recordset(typeof(Course), typeof(Lesson), typeof(CourseProgram))]
         Task<bool> UpdateCourseProgramAsync(CourseProgram courseProgram);
 
-        [Recordset(typeof(Course), typeof(Lesson), typeof(CourseProgram))]
         Task<bool> DeleteCourseProgramAsync(Guid id);
     }
 }
